85fc62cd90c9d698d29b855557e7030bf2bfeb69
Muhammad Mudassar <124991853+Mr-Mudassar@users.noreply.github.com>
2023-10-13 05:26:35 -0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,9 +2,7 @@ import { NextResponse } from 'next/server';
 
 export function middleware(request) {
   console.log('Middleware Executed');
-
-  const token = request.cookies.get('token')?.value;
-
+  
   const loginPaths = [
      '/login/UserLogin',
      '/login/SetPassword',
@@ -13,7 +11,7 @@ export function middleware(request) {
   ];
 
   // Check if the user is logged in and trying to access a login-related path
-  if (token && loginPaths.includes(request.nextUrl.pathname)) {
+  if (loginPaths.includes(request.nextUrl.pathname)) {
     return NextResponse.redirect(new URL('https://iot-jolta-final.vercel.app', request.url));
   }
   
