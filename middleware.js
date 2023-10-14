451463a1a38e01b6448474a967734c522b9a98b1
Mr-Mudassar <mudassarmuhammad776@gmail.com>
2023-10-13 18:50:12 -0700

    Login Process Added

--- middleware.js
+++ middleware.js
@@ -3,18 +3,19 @@ import { NextResponse } from 'next/server';
 export function middleware(request) {
   console.log('Middleware Executed');
 
-  const token = request.cookies.get('token')?.value;
-
   const loginPaths = [
      '/login/UserLogin',
-     '/login/SetPassword'
+     'login/Password/Reset',
+     '/login/ForgetPassword'
   ];
 
   // Check if the user is logged in and trying to access a login-related path
-  if (token && loginPaths.includes(request.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL('/', request.url));
+  if (loginPaths.includes(request.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL('https://iot-jolta-final.vercel.app', request.url));
   }
   
   // Continue processing for other routes
   return NextResponse.next();
+
+
 }
